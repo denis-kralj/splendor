f5edf38f6748dc0f50db6e1854f52e6f7ce39dea
Denis Kralj <kralj.denis.zg@gmail.com>
2019-07-14 00:33:12 +0200

    Add Noble class

---
+++ splendor-lib/Noble.cs
@@ -0,0 +1,22 @@
+namespace splendor_lib
+{
+    public class Noble
+    {
+        public Nobile(int prestige, int diamondDevelopmentPrice, int rubyDevelopmentPrice, int emeraldDevelopmentPrice, int onyxDevelopmentPrice, int sapphireDevelopmentPricet)
+        {
+            this.OnyxDevelopmentPrice = onyxDevelopmentPrice;
+            this.EmeraldDevelopmentPrice = emeraldDevelopmentPrice;
+            this.RubyDevelopmentPrice = rubyDevelopmentPrice;
+            this.DiamondDevelopmentPrice = diamondDevelopmentPrice;
+            this.SapphireDevelopmentPricet = sapphireDevelopmentPricet;
+            this.Prestige = prestige;
+        }
+
+        public int Prestige { get; }
+        public int SapphireDevelopmentPricet { get; }
+        public int DiamondDevelopmentPrice { get; }
+        public int RubyDevelopmentPrice { get; }
+        public int EmeraldDevelopmentPrice { get; }
+        public int OnyxDevelopmentPrice { get; }
+    }
+}
