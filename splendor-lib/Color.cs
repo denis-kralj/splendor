962c0ba46c3220f7bce51f41d82c7dc26facdce9
Denis Kralj <kralj.denis.zg@gmail.com>
2019-07-14 14:58:23 +0200

    Add gold token
    Add Gold token class
    Add numeric values to Color enum
    Add Yellow to Color enum for gold

--- splendor-lib/Color.cs
+++ splendor-lib/Color.cs
@@ -2,10 +2,11 @@ namespace splendor_lib
 {
     public enum Color
     {
-        Red,
-        Blue,
-        Green,
-        White,
-        Black
+        Red = 0,
+        Blue = 1,
+        Green = 2,
+        White = 4,
+        Black = 8,
+        Yellow = 16
     }
 }
---
+++ splendor-lib/Gold.cs
@@ -0,0 +1,7 @@
+namespace splendor_lib
+{
+    public class Gold : Token
+    {
+        public override Color Color => Color.Yellow;
+    }
+}
