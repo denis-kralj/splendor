7bc90c06bc90b9885e51b289cb376c7d58bd2189
Denis Kralj <kralj.denis.zg@gmail.com>
2019-07-15 19:58:16 +0200

    Remove useless field from exception class

--- splendor-lib/InsufficientTokensException.cs
+++ splendor-lib/InsufficientTokensException.cs
@@ -7,19 +7,12 @@ namespace splendor_lib
     [Serializable]
     public class InsufficientTokensException : Exception
     {
-        public IReadOnlyDictionary<Token, int> Price { get; private set; }
-
         public InsufficientTokensException()
         {
         }
 
         public InsufficientTokensException(string message) : base(message)
         {
-        }
-
-        public InsufficientTokensException(string message, IReadOnlyDictionary<Token, int> price) : base(message)
-        {
-            this.Price = price;
         }
 
         public InsufficientTokensException(string message, Exception innerException) : base(message, innerException)
