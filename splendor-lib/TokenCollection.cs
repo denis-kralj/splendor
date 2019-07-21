7527476d3faee7bdeb79927f77117ad126abe687
Denis Kralj <kralj.denis.zg@gmail.com>
2019-07-21 17:22:17 +0200

    Refactor TokenCollection class
    Remove indexer from class
    Constructor can now take initial state
    Expose SetCollectionState to users
    Modify add tokens logic
    Modify token removal, now using try-action pattern
    Refactor tests to accomodate changes

--- splendor-lib/TokenCollection.cs
+++ splendor-lib/TokenCollection.cs
@@ -6,45 +6,60 @@ namespace splendor_lib
 {
     public class TokenCollection
     {
-        private Dictionary<Token, int> _tokens;
-
-        public int TotalTokens => _tokens.Values.Sum(v => v);
-
-        public int this[Token index] => _tokens[index];
-        
-        public TokenCollection()
+        private Dictionary<Token, int> _tokensInternal;
+        public TokenCollection(int whiteCount = 0, int blackCount = 0, int blueCount = 0, int greenCount = 0, int redCount = 0, int yellowCount = 0)
         {
-            InitializeCollection();
+            SetCollectionState(whiteCount, blackCount, blueCount, greenCount, redCount, yellowCount);
         }
-
-        private void InitializeCollection()
+        public int GetCount(Token type) => _tokensInternal[type];
+        public int TotalTokens => _tokensInternal.Values.Sum(v => v);
+        public void SetCollectionState(int whiteCount = 0, int blackCount = 0, int blueCount = 0, int greenCount = 0, int redCount = 0, int yellowCount = 0)
         {
-            _tokens = new Dictionary<Token, int>
+            if(whiteCount < 0 || blackCount < 0 || blueCount < 0 || greenCount < 0 || redCount < 0 || yellowCount < 0)
+                throw new ArgumentOutOfRangeException("Token count cannot be set to a negative value");
+
+            _tokensInternal = new Dictionary<Token, int>
             {
-                { Token.White,  0 },
-                { Token.Black,  0 },
-                { Token.Blue,   0 },
-                { Token.Green,  0 },
-                { Token.Red,    0 },
-                { Token.Yellow, 0 }
+                { Token.White,  whiteCount  },
+                { Token.Black,  blackCount  },
+                { Token.Blue,   blueCount   },
+                { Token.Green,  greenCount  },
+                { Token.Red,    redCount    },
+                { Token.Yellow, yellowCount }
             };
         }
-
-        public void AddTokens(params Token[] tokens)
+        public void AddTokens(TokenCollection tokensToAdd)
         {
-            foreach(var token in tokens)
-                _tokens[token]++;
+            foreach (Token key in Enum.GetValues(typeof(Token)))
+                _tokensInternal[key] += tokensToAdd.GetCount(key);
+        }
+        public void AddTokens(Token tokenType, int amountToAdd)
+        {
+            if(amountToAdd < 0)
+                throw new ArgumentOutOfRangeException(nameof(amountToAdd), amountToAdd, "Token count cannot be negative");
+
+            _tokensInternal[tokenType] += amountToAdd;
         }
 
-        public void RemoveTokens(params Token[] tokens)
+        public bool TryTake(TokenCollection tokens)
         {
-            foreach(var token in tokens)
-            {
-                if(_tokens[token] == 0)
-                    throw new InsufficientTokensException("Not enough tokens to remove");
-                
-                _tokens[token]--;
-            }
+            foreach (Token key in Enum.GetValues(typeof(Token)))
+                if (GetCount(key) < tokens.GetCount(key))
+                    return false;
+
+            foreach (Token key in Enum.GetValues(typeof(Token)))
+                _tokensInternal[key] -= tokens.GetCount(key);
+
+            return true;
+        }
+        public bool TryTake(Token tokenType, int count)
+        {
+            if (count < 0 || GetCount(tokenType) < count)
+                return false;
+
+            _tokensInternal[tokenType] -= count;
+
+            return true;
         }
     }
 }
--- splendor-tests/TokenCollectionTests.cs
+++ splendor-tests/TokenCollectionTests.cs
@@ -14,42 +14,97 @@ namespace splendor_tests
         }
 
         [Test]
-        public void ShouldAddTokensToCollection()
+        public void ShouldAddTokensToCollectionByFirstOverride()
         {
-            _sut.AddTokens(Token.Black, Token.Black, Token.Blue);
+            var tokenCountToAdd = 3;
+            var countBefore = _sut.TotalTokens;
 
-            Assert.AreEqual(_sut.TotalTokens, 3);
+            _sut.AddTokens(new TokenCollection(whiteCount: tokenCountToAdd));
+
+            Assert.AreEqual(countBefore + tokenCountToAdd, _sut.TotalTokens);
+        }
+
+        [Test]
+        public void ShouldAddTokensToCollectionBySecondOverride()
+        {
+            var tokenCountToAdd = 5;
+            var countBefore = _sut.TotalTokens;
+
+            _sut.AddTokens(Token.Green, tokenCountToAdd);
+
+            Assert.AreEqual(countBefore + tokenCountToAdd, tokenCountToAdd);
         }
 
         [Test]
         public void ShouldRemoveTokens()
         {
-            _sut.AddTokens(Token.Black, Token.Black, Token.Blue);
+            var blackAddCount = 2;
+            var blueAddCount = 1;
+            var blackRemoveCount = 1;
+            var blueRemoveCount = 1;
+            var totalBeforeAdd = _sut.TotalTokens;
 
-            Assert.AreEqual(_sut.TotalTokens, 3);
+            _sut.AddTokens(new TokenCollection(blackCount: blackAddCount, blueCount: blueAddCount));
 
-            _sut.RemoveTokens(Token.Black, Token.Blue);
+            var totalAfterAdd = totalBeforeAdd + blackAddCount + blueAddCount;
 
-            Assert.AreEqual(_sut.TotalTokens, 1);
+            Assert.AreEqual(totalAfterAdd, _sut.TotalTokens);
+
+            Assert.IsTrue(_sut.TryTake(new TokenCollection(blackCount: blackRemoveCount, blueCount: blueRemoveCount)));
+
+            var totalAfterTake = totalAfterAdd - blackRemoveCount - blueRemoveCount;
+
+            Assert.AreEqual(totalAfterTake, _sut.TotalTokens);
         }
 
         [Test]
-        public void ShouldThrowWhenInsufficientTokens()
+        public void ShouldFailTryWhenRemovingTokensItDoesNotHave()
         {
-            Assert.Throws<InsufficientTokensException>(() => _sut.RemoveTokens(Token.Blue));
+            var redState = 0;
+            _sut.SetCollectionState(redCount: redState);
+
+            Assert.IsFalse(_sut.TryTake(Token.Red, redState + 1));
+            Assert.IsFalse(_sut.TryTake(new TokenCollection(redCount: redState + 2)));
         }
 
         [Test]
         public void CanCountSpecificTokenTypeContained()
         {
-            _sut.AddTokens(Token.Black, Token.Black, Token.Blue, Token.Red, Token.Red, Token.Yellow);
+            var blackCount = 2;
+            var blueCount = 1;
+            var greenCount = 0;
+            var redCount = 2;
+            var whiteCount = 0;
+            var yellowCount = 1;
 
-            Assert.AreEqual(_sut[Token.Black], 2);
-            Assert.AreEqual(_sut[Token.Blue], 1);
-            Assert.AreEqual(_sut[Token.Green], 0);
-            Assert.AreEqual(_sut[Token.Red], 2);
-            Assert.AreEqual(_sut[Token.White], 0);
-            Assert.AreEqual(_sut[Token.Yellow], 1);
+            _sut.AddTokens(new TokenCollection(whiteCount, blackCount, blueCount, greenCount, redCount, yellowCount));
+
+            Assert.AreEqual(_sut.GetCount(Token.Black), blackCount);
+            Assert.AreEqual(_sut.GetCount(Token.Blue), blueCount);
+            Assert.AreEqual(_sut.GetCount(Token.Green), greenCount);
+            Assert.AreEqual(_sut.GetCount(Token.Red), redCount);
+            Assert.AreEqual(_sut.GetCount(Token.White), whiteCount);
+            Assert.AreEqual(_sut.GetCount(Token.Yellow), yellowCount);
+        }
+
+        [Test]
+        public void ShouldSetupCollection()
+        {
+            var whiteCount = 3;
+            var blackCount = 5;
+            var blueCount = 1;
+            var greenCount = 0;
+            var redCount = 11;
+            var yellowCount = 6;
+
+            _sut.SetCollectionState(whiteCount, blackCount, blueCount, greenCount, redCount, yellowCount);
+
+            Assert.AreEqual(_sut.GetCount(Token.Black), blackCount);
+            Assert.AreEqual(_sut.GetCount(Token.Blue), blueCount);
+            Assert.AreEqual(_sut.GetCount(Token.Green), greenCount);
+            Assert.AreEqual(_sut.GetCount(Token.Red), redCount);
+            Assert.AreEqual(_sut.GetCount(Token.White), whiteCount);
+            Assert.AreEqual(_sut.GetCount(Token.Yellow), yellowCount);
         }
     }
 }
