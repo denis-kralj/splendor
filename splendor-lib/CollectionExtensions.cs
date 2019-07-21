86105712f9bc9beccb99b0ff50aeb7c1deb91dc4
Denis Kralj <kralj.denis.zg@gmail.com>
2019-07-21 21:25:48 +0200

    Remove unused extension method

--- splendor-lib/CollectionExtensions.cs
+++ splendor-lib/CollectionExtensions.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 
 namespace splendor_lib
 {
@@ -7,8 +6,6 @@ namespace splendor_lib
     {
         public static Stack<T> ToStack<T>(this IEnumerable<T> collection)
             => new Stack<T>(collection);
-        public static ReadOnlyDictionary<T, T2> AsReadOnly<T, T2>(this Dictionary<T, T2> source)
-            => new ReadOnlyDictionary<T, T2>(source);
         public static List<T> Pop<T>(this Stack<T> stack, uint amount)
         {
             var result = new List<T>((int)amount);
