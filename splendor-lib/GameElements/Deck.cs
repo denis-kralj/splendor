471e4cabf33925b48850e369f49cb78a194aeac6
Denis Kralj <denis-kralj@users.noreply.github.com>
2023-02-14 16:38:53 +0100

    #5 move deck utils to deck class file

--- splendor-lib/GameElements/Deck.cs
+++ splendor-lib/GameElements/Deck.cs
@@ -36,3 +36,18 @@ public class Deck<TCard>
         return _stackInternal.Pop<TCard>(count);
     }
 }
+
+internal static class DeckExtensions
+{
+    public static Stack<T> ToStack<T>(this IEnumerable<T> collection)
+        => new Stack<T>(collection);
+    public static List<T> Pop<T>(this Stack<T> stack, uint amount)
+    {
+        var result = new List<T>((int)amount);
+
+        for (int i = 0; i < amount; i++)
+            result.Add(stack.Pop());
+
+        return result;
+    }
+}
