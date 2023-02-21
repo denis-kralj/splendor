b30a5900cc6fcabd2295e5678818f5c1841cacb0
Denis Kralj <denis-kralj@users.noreply.github.com>
2023-02-21 21:03:11 +0100

    #31 Refactor take two action
    Remove chance of invalid token combination
    Update tests

--- splendor-lib/Actions/TakeTwoSameAction.cs
+++ splendor-lib/Actions/TakeTwoSameAction.cs
@@ -1,45 +1,50 @@
-using System.Linq;
-
 namespace splendor_lib;
 
 public class TakeTwoSameAction : IGameAction
 {
-    private TokenCollection _tokensInternal;
-    private const uint _tCount = 2;
+    private readonly Token _tokenType;
     private const uint _minBoardTokenCount = 4;
 
-    public TakeTwoSameAction(TokenCollection tokensToTake)
+    public TakeTwoSameAction(Token type)
     {
-        _tokensInternal = tokensToTake;
+        _tokenType = type;
     }
 
     public bool TryExecuteAction(IPlayer player, IBoard board, out ExecutionResult result)
     {
-        if (InvalidTokenCombination())
-        {
-            result = ExecutionResult.InvalidTokenCombination;
-            return false;
-        }
-
         if (BoardHasInsufficientTokens(board))
         {
             result = ExecutionResult.InsufficientTokens;
             return false;
         }
 
+        if(_tokenType == Token.Gold)
+        {
+            result = ExecutionResult.InvalidTokenCombination;
+            return false;
+        }
+
+        var tokenCollection = GenerateCollection();
+
         result = ExecutionResult.Success;
-        player.CollectTokens(_tokensInternal);
-        return board.TryTakeTokensFormBoard(_tokensInternal);
+        player.CollectTokens(tokenCollection);
+        return board.TryTakeTokensFormBoard(tokenCollection);
+    }
+
+    private TokenCollection GenerateCollection()
+    {
+        switch(_tokenType)
+        {
+            case Token.Onyx: return new TokenCollection(onyxCount: 2);
+            case Token.Diamond: return new TokenCollection(diamondCount: 2);
+            case Token.Emerald: return new TokenCollection(emeraldCount: 2);
+            case Token.Sapphire: return new TokenCollection(sapphireCount: 2);
+            case Token.Ruby: default: return new TokenCollection(rubyCount: 2);
+        }
     }
 
     private bool BoardHasInsufficientTokens(IBoard board)
     {
-        var color = Tokens.AllTokens.First(t => _tokensInternal.GetCount(t) == _tCount);
-
-        return board.BoardTokens.GetCount(color) < _minBoardTokenCount;
+        return board.BoardTokens.GetCount(_tokenType) < _minBoardTokenCount;
     }
-
-    private bool InvalidTokenCombination() =>
-        _tokensInternal.TotalTokens != _tCount ||
-        !Tokens.AllTokens.Any(t => _tokensInternal.GetCount(t) == _tCount);
 }
--- splendor-tests/TakeTwoSameActionTests.cs
+++ splendor-tests/TakeTwoSameActionTests.cs
@@ -24,22 +24,10 @@ public class TakeTwoSameActionTests
         var tokensToTake = new TokenCollection(0, 0, 2, 0, 0, 0);
         var player = new Player("Shaggy");
         var board = new GameBoard(PlayerCount.Two, _nobles, _developments);
-        var sut = new TakeTwoSameAction(tokensToTake);
+        var sut = new TakeTwoSameAction(Token.Ruby);
 
         Assert.IsTrue(sut.TryExecuteAction(player, board, out var result));
         Assert.AreEqual(ExecutionResult.Success, result);
-    }
-
-    [Test]
-    public void DoesntExecuteWithInvalidTokenCount()
-    {
-        var tokensToTake = new TokenCollection(1, 1, 0, 0, 0, 0);
-        var player = new Player("Shaggy");
-        var board = new GameBoard(PlayerCount.Two, _nobles, _developments);
-        var sut = new TakeTwoSameAction(tokensToTake);
-
-        Assert.IsFalse(sut.TryExecuteAction(player, board, out var result));
-        Assert.AreEqual(ExecutionResult.InvalidTokenCombination, result);
     }
 
     [Test]
@@ -48,7 +36,7 @@ public class TakeTwoSameActionTests
         var tokensToTake = new TokenCollection(0, 0, 2, 0, 0, 0);
         var player = new Player("Shaggy");
         var board = new GameBoard(PlayerCount.Two, _nobles, _developments);
-        var sut = new TakeTwoSameAction(tokensToTake);
+        var sut = new TakeTwoSameAction(Token.Sapphire);
         board.TryTakeTokensFormBoard(new TokenCollection(4, 4, 4, 4, 4, 5));
 
         Assert.IsFalse(sut.TryExecuteAction(player, board, out var result));
@@ -61,7 +49,7 @@ public class TakeTwoSameActionTests
         var tokensToTake = new TokenCollection(0, 0, 2, 0, 0, 0);
         var player = new Player("Shaggy");
         var board = new GameBoard(PlayerCount.Two, _nobles, _developments);
-        var sut = new TakeTwoSameAction(tokensToTake);
+        var sut = new TakeTwoSameAction(Token.Sapphire);
         board.TryTakeTokensFormBoard(new TokenCollection(0, 0, 1, 0, 0, 0));
 
         Assert.IsFalse(sut.TryExecuteAction(player, board, out var result));
